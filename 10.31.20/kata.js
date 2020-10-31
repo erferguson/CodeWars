7953dfe926fa321034b5595a4d118dc5958e8a85
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-10-31 12:00:07 -0700

    10.31.20 - kata.js 2

--- 10.31.20/kata.js
+++ 10.31.20/kata.js
@@ -12,3 +12,14 @@ function squareSum(numbers){
     })
     return sum
 }
+
+// Create a function that returns true when num1 is equal to num2; 
+// otherwise return false.
+
+function isSameNum(num1, num2) {
+	if (num1 === num2){
+        return true
+    }
+    else return false
+}
+
