81339df7d4d1c6b2f5f11e4ecd7a8b09002ff4f6
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-11-25 18:12:34 -0800

    11.25.20 - #1, solution passed

---
+++ 11.25.20/kata.js
@@ -0,0 +1,21 @@
+// In this Kata we are passing a number (n) into a function.
+
+// Your code will determine if the number passed is even 
+// (or not).
+
+// The function needs to return either a true or false.
+
+// Numbers may be positive or negative, integers or floats.
+
+// Floats are considered UNeven for this kata.
+
+// given >> integer
+// return >> integer + true/false
+
+function testEven(n) {
+    if(n % 2 === 0){
+        return true
+    } else {
+        return false
+    }
+}
