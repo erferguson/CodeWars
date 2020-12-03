fc876a97c9f1501cfe537fccc63211969fe19565
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-12-02 20:28:38 -0800

    12.2.20 - #2 kata, solution passes

--- 12.2.20/kata.js
+++ 12.2.20/kata.js
@@ -25,3 +25,49 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
   // given > integers
   // return > boolean, True/False
 
+// >>>>>>
+
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better 
+// than the average student in your class.
+
+// You receive an array with your peers' test scores. 
+// Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)
+
+function betterThanAverage(classPoints, yourPoints) {
+    let sum = 0
+    
+    for(let i = 0; i < classPoints.length; i++){
+      sum += classPoints[i]
+    }
+
+    let averagePoints = sum / classPoints.length
+
+    if(yourPoints > averagePoints ){
+        return true
+    } else {
+        return false
+    }
+    
+  }
+  
+
+  // given > array of integers
+  // return > boolean, True/False
+
+  // calculate average test score of classPoints
+
+
+  // compare to yourPoints
+  //    if yourPoints higher than classPoints, return True
+
+  // .length to find length of array, classPoints  ?
+
+  // for(let i = 0; i < classPoints.length; i++){
+      // sum += classPoints[i]
+      // return sum / classPoints.length
+  // }
+
