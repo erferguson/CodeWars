7f7ca0a31f38ed7dce6526c35f9092f1320a85ae
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-12-05 11:26:32 -0800

    12.5.20 - #1 kata, solution passed

---
+++ 12.5.20/kata.js
@@ -0,0 +1,12 @@
+// Write a function called repeat_str which repeats the 
+// given string src exactly count times.
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
+// n > number of times to repeat
+// s > is the string
+
+// repeat()
+
+// s.repeat(n)
