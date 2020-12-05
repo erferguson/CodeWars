5e410a38a0193dca1605c291d608806aefa34a4b
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-12-05 11:50:49 -0800

    12.5.20 - #2, solution passes

--- 12.5.20/kata.js
+++ 12.5.20/kata.js
@@ -10,3 +10,19 @@ function repeatStr (n, s) {
 // repeat()
 
 // s.repeat(n)
+////
+// In this kata you will create a function
+// that takes a list of non-negative integers
+// and strings and returns a new list with the
+// strings filtered out.
+
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+
+function filter_list(l) {
+    // Return a new array with the strings 
+    // filtered out
+    return l.filter(x => typeof x === 'number')
+  }
+
+// filter()
