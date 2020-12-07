139c42101d48ffc2cc4d3970b700b308022fe3e3
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-12-07 14:44:01 -0800

    12.7.20 - 1st kata set up

---
+++ 12.7.20/kata.js
@@ -0,0 +1,14 @@
+// Take an array and remove every second element from the array. 
+// Always keep the first element and start removing with the 
+// next element.
+
+// Example:
+
+// myArr = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...];
+// None of the arrays will be empty
+
+function removeEveryOther(arr){
+    //your code here
+  }
+
+  
