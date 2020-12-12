9afd26ef18d952c01fd6e5743cd918da63917981
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-12-11 21:21:12 -0800

    12.11.20 - #1 solution passes but needed help

---
+++ 12.11.20/kata.js
@@ -0,0 +1,17 @@
+// Given an index and an array, 
+// return the value of the array with the given index.
+
+// Examples
+// valueAt([1, 2, 3, 4, 5, 6], 10 / 2) ➞ 6
+
+// valueAt([1, 2, 3, 4, 5, 6], 8.0 / 2) ➞ 5
+
+// valueAt([1, 2, 3, 4], 6.535355314 / 2) ➞ 4
+
+function valueAt(arr, i) {
+	return arr[Math.floor(i)]
+}
+
+// given > integers
+// return > integer
+
