ce3f39ca67f85953e955c3e241afeb911d77935f
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-12-20 12:20:33 -0800

    12.20.20 > #1, solution passed

---
+++ 12.20.20/kata.js
@@ -0,0 +1,18 @@
+// create a function that removes the first and last characters of a string. 
+// You're given one parameter, the original string. 
+// You don't have to worry with strings with less than two characters.
+
+function removeChar(str){
+    let newString = str.substr(1).slice(0,-1)
+    return newString
+   };
+
+   // given > string
+   // return > string
+
+   // .spilt('') > break the string into individual letters?
+   // let spiltStr = str.spilt('')
+
+   // remove first and last, how?
+
+
